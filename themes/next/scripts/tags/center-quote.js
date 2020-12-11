66e8c4253833f95e5b7a98ef669b82434e17214d
沈一州 <shenyizhou@corp.netease.com>
2020-12-11 16:45:57 +0800

    feat: 更新 next 架构

--- themes/next/scripts/tags/center-quote.js
+++ themes/next/scripts/tags/center-quote.js
@@ -1,19 +1,11 @@
 /**
- * center-quote.js | global hexo script.
- *
- * Usage:
- *
- * {% centerquote %}Something{% endcenterquote %}
- * {% cq %}Something{% endcq %}
+ * center-quote.js | https://theme-next.js.org/docs/tag-plugins/
  */
 
 'use strict';
 
-function centerQuote (args, content) {
-  return '<blockquote class="blockquote-center">' +
-            hexo.render.renderSync({text: content, engine: 'markdown'}) +
-          '</blockquote>';
-}
-
-hexo.extend.tag.register('centerquote', centerQuote, {ends: true});
-hexo.extend.tag.register('cq', centerQuote, {ends: true});
+module.exports = ctx => function(args, content) {
+  return `<blockquote class="blockquote-center">
+${ctx.render.renderSync({ text: content, engine: 'markdown' })}
+</blockquote>`;
+};
