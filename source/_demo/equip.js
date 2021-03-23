6a538f7aafa5e191427b777c44a5aa7847098731
沈一州 <shenyizhou@corp.netease.com>
2021-03-24 03:39:43 +0800

    feat: 王者出装

---
+++ source/_demo/equip.js
@@ -0,0 +1,31 @@
+function getPer(per = 50) {
+    [
+        ...document.getElementsByClassName('vxe-table--body')[3].children[1]
+            .children
+    ]
+        .map((child) => {
+            const arr = [...child.children];
+            const img = `padding: 10px 10px;background-image: url(${arr[1].children[0].children[0].src});background-size: contain;background-repeat: no-repeat;color: transparent;`;
+            const perUse = Number(arr[4].children[0].children[0].innerHTML);
+            const perWin = Number(arr[5].children[0].children[0].innerHTML);
+            const perOppoLose =
+                (per - (perUse * perWin) / 100) /
+                (1 - Math.min(perUse, 99.99) / 100);
+            return {
+                img,
+                p: perOppoLose
+            };
+        })
+        .sort((a, b) => {
+            if (a.p < b.p) {
+                return -1;
+            }
+            if (a.p > b.p) {
+                return 1;
+            }
+            return 0;
+        })
+        .forEach(({ img, p }) => {
+            console.log('%c+', img, `${(100 - p).toFixed(2)}%`);
+        });
+}
